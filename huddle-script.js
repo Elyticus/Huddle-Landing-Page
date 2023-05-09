02f7e243f19024e393e29a99ed0fa75a8252c344
Pirvulescu Catalin <125762622+Elyticus@users.noreply.github.com>
2023-05-09 22:54:24 +0300

    small updates plus JS

---
+++ huddle-script.js
@@ -0,0 +1,10 @@
+// const freeButtonELement = document.querySelector(".free__btn");
+// const headerFlex = document.querySelector(".header__flex");
+
+// const thankYouMessage = document.querySelector(".thank__message");
+// console.log(freeButtonELement);
+
+// freeButtonELement.addEventListener("click", () => {
+//   headerFlex.classList.add("header__js");
+//   thankYouMessage.style.display = "block";
+// });
